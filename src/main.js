10f7598bd454975f6e9bf5644bb1f74c72f8b9a5
Di Zhou <chaodyz@gmail.com>
2019-03-10 23:22:15 -0400

    ci(fontawesome): import font awesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
+import '@fortawesome/fontawesome-free/css/all.css'; // Ensure you are using css-loader
 
-Vue.use(Vuetify);
+// main.styl
+
+Vue.use(Vuetify, {
+  iconfont: 'fa',
+});
 Vue.config.productionTip = false;
 
 new Vue({
