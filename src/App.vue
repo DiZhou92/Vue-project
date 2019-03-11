8caac19475f1663a89036d9c59a8d10acb0945d8
Di Zhou <chaodyz@gmail.com>
2019-03-10 23:24:06 -0400

    feat(footer): add sitefooter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <list name="testname"></list>
+    <site-footer></site-footer>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import List from "./components/list";
+import SiteFooter from "./containers/site-footer";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    List,
+    SiteFooter
   }
 };
 </script>
---
+++ src/components/list.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="list">
+    <span>{{name}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "list",
+  props: {
+    name
+  }
+};
+</script>
+
+<style>
+#list {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid lemonchiffon;
+}
+</style>
